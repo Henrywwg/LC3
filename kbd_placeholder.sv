5e310841a57ef317f326629d8bfbf4b933bfb872
Avery <henryjwg@gmail.com>
2024-09-01 14:49:51 -0500

    fixed it

--- kbd_placeholder.sv
+++ kbd_placeholder.sv
@@ -15,11 +15,11 @@ module kbd_placeholder(
         else if (LD)
             {ps2_data, SHIFT_REG} <= {1'b1, 1'b0, data};
         else
-            {SHIFT_REG[10:1]} <= {SHIFT_REG[9:0], 1'b1};    //Shift left and shift 1s into right side
+            {SHIFT_REG[10:0]} <= {SHIFT_REG[9:0], 1'b1};    //Shift left and shift 1s into right side
     end
 
 
-    assign PS2_data = SHIFT_REG[10];
+    assign ps2_data = SHIFT_REG[10];
 
 
 
