9cc60cdc62b0165d3b51fafa7102c9210886017f
Henry Wysong-Grass <hwysonggrass@wisc.edu>
2024-09-05 21:38:30 -0500

    adding testbench files for text display modules

---
+++ template_tb.sv
@@ -0,0 +1,27 @@
+module TEMPLATE_tb();
+    logic clk, rst_n;
+
+    
+
+
+    //Instantiate DUT(s)
+    LC3 iDUT(.clk(clk), .rst_n(rst_n), OTHER HOOKUPS);
+
+    initial begin
+        clk = 0;
+        rst_n = 0;
+        //PUT OTHER SIGNALS TO INIT
+
+        repeat(2) @(negedge clk);
+        rst_n = 1;
+        
+        
+        
+
+        $stop();
+    end
+
+    //Base system clock (10ns period)
+    always
+        #5 clk = ~clk;
+endmodule
